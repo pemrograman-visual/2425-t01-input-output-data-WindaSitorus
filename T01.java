c094e0eca853470aaf63a0094c061cb15b3a3ea8
WindaSitorus <windasitorus0197@gmail.com>
2024-09-06 14:52:36 +0700

    sudah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -7,20 +7,20 @@ public class T01 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String iSBN, judul, penulis, penerbit, formatbuku;
+        String isbn, judul, penulis, penerbit, formatbuku;
         int tahunterbit, stok;
         double hargapembelian, minimummargin, rating;
 
-        iSBN = input.nextLine();
+        isbn = input.nextLine();
         judul = input.nextLine();
         penulis = input.nextLine();
+        tahunterbit = Integer.parseInt(input.nextLine());
         penerbit = input.nextLine();
         formatbuku = input.nextLine();
-        tahunterbit = Integer.parseInt(input.nextLine());
-        stok = Integer.parseInt(input.nextLine());
         hargapembelian = Double.parseDouble(input.nextLine());
         minimummargin = Double.parseDouble(input.nextLine());
+        stok = Integer.parseInt(input.nextLine());
         rating = Double.parseDouble(input.nextLine());
-        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + penerbit + "|" + formatbuku + "|" + tahunterbit + "|" + stok + "|" + hargapembelian + "|" + minimummargin + "|" + rating);
+        System.out.println(isbn + "|" + judul + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbuku + "|" + hargapembelian + "|" + minimummargin + "|" + stok + "|" + rating);
     }
 }
